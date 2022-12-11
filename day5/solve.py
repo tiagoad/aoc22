17cf047ec9c7dbbac54fdbf5b39c3acd2b49de53
Tiago Dias <tiago@tdias.tech>
2022-12-11 19:35:06 +0000

    day5: part 2

--- day5/solve.py
+++ day5/solve.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def main():
     with open('input.txt') as f:
         stacks = []
@@ -15,22 +18,35 @@ def main():
 
                     stacks[i].append(l[ci])
 
+        p1stacks = copy.deepcopy(stacks)
+        p2stacks = copy.deepcopy(stacks)
+
         for l in f:
             spl = l.split(" ")
             it = int(spl[1])
             a = int(spl[3]) - 1
             b = int(spl[5]) - 1
 
-            stacks[b] = list(reversed(stacks[a][:it])) + stacks[b]
-            stacks[a] = stacks[a][it:]
+            # part 1
+            p1stacks[b] = list(reversed(p1stacks[a][:it])) + p1stacks[b]
+            p1stacks[a] = p1stacks[a][it:]
 
-        output = ""
-        for s in stacks:
+            # part 2
+            p2stacks[b] = list(p2stacks[a][:it]) + p2stacks[b]
+            p2stacks[a] = p2stacks[a][it:]
+
+        p1output = ""
+        for s in p1stacks:
             if len(s) > 0:
-                output += s[0]
+                p1output += s[0]
 
-        print(output)
+        p2output = ""
+        for s in p2stacks:
+            if len(s) > 0:
+                p2output += s[0]
 
+        print("[P1]", p1output)
+        print("[P2]", p2output)
 
 
 if __name__ == "__main__":
