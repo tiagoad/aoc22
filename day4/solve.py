baa633e1c764ca2e8b3e614a66f2167f417ed230
Tiago Dias <tiago@tdias.tech>
2022-12-11 19:06:18 +0000

    day4: part 2

--- day4/solve.py
+++ day4/solve.py
@@ -1,6 +1,7 @@
 def main():
     with open('input.txt') as f:
         overlap_total = 0
+        overlap_partial = 0
 
         for l in f:
             l = l[:-1]
@@ -12,7 +13,11 @@ def main():
             if (a1 >= b1 and a2 <= b2) or (b1 >= a1 and b2 <= a2):
                 overlap_total += 1
 
-        print("Total overlaps:", overlap_total)
+            if (b2 >= a1 >= b1) or (a2 >= b1 >= a1):
+                overlap_partial += 1
+
+        print("[P1] Total overlaps:", overlap_total)
+        print("[P2] Partial overlaps:", overlap_partial)
 
 
 if __name__ == "__main__":
