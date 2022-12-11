33b171b3c81e2bd96cae4e5ebf6dfa4affea130a
Tiago Dias <tiago@tdias.tech>
2022-12-11 18:57:07 +0000

    day3: part 2

--- day3/solve.py
+++ day3/solve.py
@@ -1,6 +1,5 @@
 ORD_A = ord('A')
 ORD_a = ord('a')
-FOUND_ARR = [False] * 53
 
 
 def prio(n):
@@ -8,28 +7,59 @@ def prio(n):
     return (on - ORD_A + 26 if on < ORD_a else on - ORD_a) + 1
 
 
+def p1(l):
+    mid = len(l) // 2
+    a, b = l[:mid], l[mid:]
+
+    found = [False] * 53
+
+    for it in a:
+        ip = prio(it)
+        found[ip] = True
+
+    for it in b:
+        ip = prio(it)
+        if found[ip]:
+            return ip
+
+    return 0
+
+
+def p2(ls):
+    found = [0] * 53
+
+    for li, l in enumerate(ls, 1):
+        for it in l:
+            ip = prio(it)
+            if li == 1 or found[ip] == li:
+                if li == 3:
+                    return ip
+                else:
+                    found[ip] = li + 1
+
+    return 0
+
+
 def main():
     with open("input.txt") as f:
-        total = 0
+        p1total = 0
+        p2total = 0
 
+        ls = []
         for l in f:
             l = l[:-1]
-            mid = len(l) // 2
-            a, b = l[:mid], l[mid:]
 
-            found = [*FOUND_ARR]
+            # part 1
+            p1total += p1(l)
 
-            for it in a:
-                ip = prio(it)
-                found[ip] = True
+            # part 2
+            ls.append(l)
+            if len(ls) == 3:
+                p2total += p2(ls)
+                ls = []
 
-            for it in b:
-                ip = prio(it)
-                if found[ip]:
-                    total += ip
-                    break
-
-        print("Total priorities:", total)
+        print("[P1] Total priorities:", p1total)
+        print("[P2] Total priorities:", p2total)
 
 
 if __name__ == "__main__":
