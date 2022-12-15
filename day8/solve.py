1f1c2a400c03dc0e56a155856468f071f7ec7c96
Tiago Dias <tiago@tdias.tech>
2022-12-15 03:17:47 +0000

    day8: part 2

--- day8/solve.py
+++ day8/solve.py
@@ -13,9 +13,11 @@ def get_map():
 
 def find_visibility(m):
     w = m.shape[0]
-    vis = np.zeros(m.shape)
+    vis = np.zeros(m.shape, dtype=int)
+    vdis = np.zeros((4, *m.shape), dtype=int)
 
     side_max_value = np.zeros((4,))
+    side_last_pos = np.zeros((4, 10))
 
     # the map is square, so we can easily sweep from all sides
     # in the same loop. these functions convert the a and b to
@@ -33,6 +35,7 @@ def find_visibility(m):
 
     for a in range(w):
         side_max_value.fill(-1)
+        side_last_pos.fill(0)
 
         for b in range(w):
             for si, sfn in enumerate(side_coord_get):
@@ -42,13 +45,19 @@ def find_visibility(m):
                     vis[x, y] += 1
                     side_max_value[si] = val
 
-    return vis
+                vdis[si, x, y] = b - side_last_pos[si][val]
+                side_last_pos[si][: val + 1] = b
+
+    scn = np.prod(vdis, axis=0)
+
+    return vis, scn
 
 
 def main():
     m = get_map()
-    v = find_visibility(m)
-    print(np.count_nonzero(v))
+    v, s = find_visibility(m)
+    print("[P1]", np.count_nonzero(v))
+    print("[P2]", np.amax(s))
 
 
 if __name__ == "__main__":
