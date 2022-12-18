fa5bbb3dda25fcb9e73d5d8651dab10bbe80d9ad
Tiago Dias <tiago@tdias.tech>
2022-12-18 14:11:10 +0000

    day13: part 2

--- day13/solve.py
+++ day13/solve.py
@@ -1,3 +1,4 @@
+import functools
 import itertools
 import json
 
@@ -5,9 +6,9 @@ import json
 def compare(a, b):
     if type(a) == int and type(b) == int:
         if a == b:
-            return None
+            return 0
         else:
-            return a < b
+            return -1 if a < b else 1
 
     elif type(a) == int:
         a = [a]
@@ -17,21 +18,23 @@ def compare(a, b):
 
     for a2, b2 in zip(a, b):
         r = compare(a2, b2)
-        if r is not None:
+        if r != 0:
             return r
 
     if len(a) > len(b):
-        return False
+        return 1
 
     elif len(a) < len(b):
-        return True
+        return -1
 
     else:
-        return None
+        return 0
 
 
 def main():
     right_pairs = []
+    dividers = [[[2]], [[6]]]
+    pairs = dividers.copy()
 
     with open("input.txt") as f:
         for i in itertools.count(start=1):
@@ -41,10 +44,22 @@ def main():
             if a == "":
                 break
 
-            if compare(json.loads(a), json.loads(b)):
+            a = json.loads(a)
+            b = json.loads(b)
+
+            if compare(a, b) == -1:
                 right_pairs.append(i)
 
+            pairs += [a, b]
+
+    pairs.sort(key=functools.cmp_to_key(compare))
+    s = 1
+    for i, l in enumerate(pairs, start=1):
+        if l in dividers:
+            s *= i
+
     print("[P1]", sum(right_pairs))
+    print("[P2]", s)
 
 
 if __name__ == "__main__":
